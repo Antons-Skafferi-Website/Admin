2837ce4e1a3b284e7cccaf7ccaa756c034c46cc0
feny1800 <feny1800@student.miun.se>
2021-02-24 11:49:57 +0100

    login functionality
    login function to admin.xhtml
    replaced index.xhtml with login.xhtml

---
+++ src/main/java/login/LoginDAO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ *
+ * @author Felix
+ */
+public class LoginDAO {
+
+	public static boolean validate(String user, String password) {
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			con = DataConnect.getConnection();
+			ps = con.prepareStatement("Select uname, password from APP.USERS where uname = ? and password = ?");
+			ps.setString(1, user);
+			ps.setString(2, password);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				//result found, means valid inputs
+                                System.out.println("Login succeded");
+				return true;
+			}
+		} catch (SQLException ex) {
+			System.out.println("Login error -->" + ex.getMessage());
+			return false;
+		} finally {
+			DataConnect.close(con);
+		}
+		return false;
+	}
+}
